e5ca2b3cd1a6c8661848f497be82e7f60dee7c7f
JuHyeong424 <wkdwngud963@gmail.com>
2025-12-21 01:24:48 +0900

    feat: 스타일 설정 및 페이지 설정하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="container mx-auto p-4">
       <Header />
     </div>
   );
