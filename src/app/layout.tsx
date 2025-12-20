f6e72d8aa68c61f916e5900ec262d24e28167c26
JuHyeong424 <wkdwngud963@gmail.com>
2025-12-21 01:22:57 +0900

    style: 배경 및 폰트 스타일 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,23 @@
+import "@/app/globals.css";
+import { Inter, Fira_Code } from "next/font/google";
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: "--font-inter",
+})
+
+const firaCode = Fira_Code({
+  subsets: ['latin'],
+  variable: "--font-fira-code",
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable} ${firaCode.variable}`}>
       <body>
         {children}
       </body>
