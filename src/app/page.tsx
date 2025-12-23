c8fae9c4ebb1466ee46f7150e703e567e5ba6808
JuHyeong424 <wkdwngud963@gmail.com>
2025-12-24 03:03:09 +0900

    style: Header 섹션 위치 고정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import Introduction from "@/sections/Introduction/Introduction";
 export default function Home() {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
-      <Header />
+      <div className="fixed top-0 left-0 right-0 z-50 h-16 px-6 lg:px-24 py-6">
+        <Header />
+      </div>
       <main className="flex-1 flex flex-col justify-center px-10 lg:px-24">
         <Introduction />
       </main>
