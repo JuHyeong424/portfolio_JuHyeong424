52527e82b9118a3a6f776dd58777767286918373
JuHyeong424 <wkdwngud963@gmail.com>
2025-12-24 02:49:21 +0900

    feat: 다양한 웹뷰 버전 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
       <Header />
-      <main className="flex-1 flex flex-col justify-center px-24">
+      <main className="flex-1 flex flex-col justify-center px-10 lg:px-24">
         <Introduction />
       </main>
     </div>
--- src/sections/Introduction/Introduction.tsx
+++ src/sections/Introduction/Introduction.tsx
@@ -12,35 +12,37 @@ export default function Introduction() {
   return (
     <section aria-label="Introduction" className="flex flex-col gap-4">
       <div className="relative z-20">
-        <p className="text-light-coral font-fira-code mb-6">안녕하세요, 저는</p>
-        <p className="text-white/95 font-bold text-8xl">장주형입니다</p>
-        <p className="text-5xl font-bold my-4">
+        <p className="text-light-coral font-fira-code lg:mb-6 mb-2">안녕하세요, 저는</p>
+        <p className="text-white/95 font-bold xl:text-8xl lg:text-7xl md:text-6xl text-xl">장주형입니다</p>
+        <p className="xl:text-5xl lg:text-4xl md:text-2xl text-xs font-bold my-4">
           사용자 경험을 설계하는 프론트엔드 개발자
         </p>
-        <p className="mt-2 my-6">
+        <p className="mt-2 my-6 md:text-xs text-[10px]">
           사용자의 흐름을 먼저 고민하며,
           <span className="font-bold"> React</span>와
           <span className="font-bold"> TypeScript</span>로 이해하기 쉬운 UI를
           만듭니다.
         </p>
-        <div className="flex gap-4">
+        <div className="flex md:flex-row flex-col gap-4">
           <a
             href={URL.github}
             target={URL.blank}
             className="
             flex flex-row justify-center items-center gap-2
-            border-2 rounded-lg border-light-coral px-auto py-5
-            w-40 font-fira-code text-sm text-light-coral
+            border-2 rounded-lg border-light-coral px-auto xl:py-5
+            xl:w-40 font-fira-code lg:text-sm text-light-coral
             group hover-glass
+            lg:w-35 lg:py-4 md:w-35 md:py-3
+            py-2 text-xs
           "
           >
             <FaGithub />
             <span className="text-gradient-coral">GitHub</span>
             <MdOutlineArrowOutward
               className="
-            transition-transform duration-200 
-            group-hover:translate-x-1 
-            group-hover:-translate-y-1
+              transition-transform duration-200 
+              group-hover:translate-x-1 
+              group-hover:-translate-y-1
             "
             />
           </a>
@@ -49,18 +51,20 @@ export default function Introduction() {
             target={URL.blank}
             className="
             flex flex-row justify-center items-center gap-2
-            border border-navy rounded-lg bg-light-coral px-auto py-5
-            w-40 font-fira-code text-sm text-navy
+            border border-navy rounded-lg bg-light-coral px-auto xl:py-5
+            xl:w-40 font-fira-code lg:text-sm text-navy
             group hover-glass hover:bg-dark-coral
+            lg:w-35 lg:py-4 md:w-35 md:py-3
+            py-2 text-xs
           "
           >
             <RiArticleLine />
             Blog
             <MdOutlineArrowOutward
               className="
-            transition-transform duration-200 
-            group-hover:translate-x-1 
-            group-hover:-translate-y-1
+              transition-transform duration-200 
+              group-hover:translate-x-1 
+              group-hover:-translate-y-1
             "
             />
           </a>
@@ -73,10 +77,10 @@ export default function Introduction() {
         <span className="absolute top-[78vh] left-[20vw] w-2.5 h-2.5 rounded-full animate-normal-blink" />
       </div>
 
-      <div className="fixed bottom-[15vh] left-1/2 -translate-x-1/2 z-50 flex flex-col items-center">
-        <span className="text-[10px] mb-2 font-fira-code">Scroll Down</span>
-        <div className="relative w-5 h-10 border-2 rounded-2xl">
-          <span className="absolute top-2 left-1/2 -translate-x-1/2 w-1 h-3.5 bg-light-coral rounded-full animate-float" />
+      <div className="fixed bottom-[5vh] xl:bottom-[15vh] md:bottom-[10vh] left-1/2 -translate-x-1/2 z-50 flex flex-col items-center">
+        <span className="text-[6px] xl:text-[10px] md:text-[9px] mb-2 font-fira-code">Scroll Down</span>
+        <div className="relative w-2 h-6 md:w-5 md:h-10 border-2 rounded-2xl">
+          <span className="absolute top-2 left-1/2 -translate-x-1/2 w-0.5 md:w-1 h-2 md:h-3.5 bg-light-coral rounded-full animate-float" />
         </div>
       </div>
     </section>
