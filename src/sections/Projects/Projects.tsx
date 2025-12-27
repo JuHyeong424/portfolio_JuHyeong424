acac5e6dd0ccd59cb5b707349e92fcdb4a384be2
JuHyeong424 <wkdwngud963@gmail.com>
2025-12-28 03:01:14 +0900

    feat: 프로젝트 페이지 넘김 framer motion 처리

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -3,8 +3,10 @@
 import Title from "@/components/common/Title";
 import { projects } from "@/app/data/projects";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import ProjectDetail from "@/sections/Projects/components/ProjectDetail";
 import PagingButton from "@/sections/Projects/components/PagingButton";
+import { sliderVariants } from "@/sections/Projects/animation";
 
 const title = {
   num: "03",
@@ -13,24 +15,44 @@ const title = {
 
 export default function Projects() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [direction, setDirection] = useState<1 | -1>(1);
   const currentProject = projects[currentIndex];
 
   return (
     <main id="Projects" className="relative h-screen snap-start">
       <Title num={title.num} name={title.name} />
-
-      <section
-        className="
+      <section className="
         flex w-full relative
         xl:top-42 xl:gap-8
         lg:top-40 lg:gap-6
         md:top-40 md:gap-6
         top-32 gap-4 flex-col
-    "
-      >
-        <ProjectDetail currentIndex={currentIndex} currentProject={currentProject} />
-        <PagingButton total={projects.length} setCurrentIndex={setCurrentIndex} />
+      ">
+        <AnimatePresence mode="wait" custom={direction}>
+          <motion.div
+            key={currentIndex}
+            custom={direction}
+            variants={sliderVariants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              duration: 0.45,
+              ease: "easeOut",
+            }}
+          >
+            <ProjectDetail 
+              currentIndex={currentIndex} 
+              currentProject={currentProject} 
+            />
+          </motion.div>
+        </AnimatePresence>
       </section>
+        <PagingButton 
+          setDirection={setDirection} 
+          total={projects.length} 
+          setCurrentIndex={setCurrentIndex} 
+        />
     </main>
   );
 }
--- src/sections/Projects/components/PagingButton.tsx
+++ src/sections/Projects/components/PagingButton.tsx
@@ -3,25 +3,28 @@ import { GrFormNext } from "react-icons/gr";
 import type { Dispatch, SetStateAction } from "react";
 
 interface PagingButtonProps {
+  setDirection: Dispatch<SetStateAction<-1 | 1>>;
   total: number;
   setCurrentIndex: Dispatch<SetStateAction<number>>;
 }
 
-export default function PagingButton({ total, setCurrentIndex }: PagingButtonProps) {
+export default function PagingButton({ setDirection, total, setCurrentIndex }: PagingButtonProps) {
   const handlePrev = () => {
-      setCurrentIndex((prev: number) => {
-        return prev === 0 ? total - 1 : prev - 1;
-      });
-    };
+    setDirection(-1);
+    setCurrentIndex((prev: number) => {
+      return prev === 0 ? total - 1 : prev - 1;
+    });
+  };
   
-    const handleNext = () => {
-      setCurrentIndex((prev: number) => {
-        return prev === total - 1 ? 0 : prev + 1;
-      });
-    };
+  const handleNext = () => {
+    setDirection(1);
+    setCurrentIndex((prev: number) => {
+      return prev === total - 1 ? 0 : prev + 1;
+    });
+  };
 
   return (
-    <aside>
+    <aside className="absolute inset-0">
       <button
         aria-label="previous button"
         onClick={handlePrev}
@@ -29,8 +32,8 @@ export default function PagingButton({ total, setCurrentIndex }: PagingButtonPro
           absolute cursor-pointer hover:border-light-coral hover:text-light-coral
           flex items-center justify-center border rounded-full w-12 h-12
           lg:-left-24 lg:top-1/2 lg:-translate-y-1/2
-          md:-bottom-12 md:left-1/3
-          -bottom-12 left-1/5
+          md:bottom-12 md:left-1/3
+          bottom-12 left-1/5
         ">
         <GrFormPrevious />
       </button>
@@ -40,9 +43,9 @@ export default function PagingButton({ total, setCurrentIndex }: PagingButtonPro
         className="
           absolute cursor-pointer hover:border-light-coral hover:text-light-coral
           flex items-center justify-center border rounded-full w-12 h-12
-          lg:-right-24 lg:top-1/2 lg:-translate-y-1/2
-          md:-bottom-12 md:right-1/3
-          -bottom-12 right-1/5
+          lg:-right-24 lg:top-1/2 lg:-translate-y-1/2 pointer-events-auto
+          md:bottom-12 md:right-1/3
+          bottom-12 right-1/5
         ">
         <GrFormNext />
       </button>
