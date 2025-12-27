ce23e160e07b213294b953453d5300fc7f28b7e1
JuHyeong424 <wkdwngud963@gmail.com>
2025-12-28 00:27:35 +0900

    feat: 각 프로젝트 이동 버튼 추가

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import Title from "@/components/common/Title";
 import { projects } from "@/app/data/projects";
 import { CiCalendar } from "react-icons/ci";
 import { AiOutlineTeam } from "react-icons/ai";
 import { IoChevronForward } from "react-icons/io5";
 import { FaGithub } from "react-icons/fa";
+import { GrFormPrevious } from "react-icons/gr";
+import { GrFormNext } from "react-icons/gr";
+import { useState } from "react";
 
 const title = {
   num: "03",
@@ -11,90 +16,125 @@ const title = {
 };
 
 export default function Projects() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentProject = projects[currentIndex];
+
+  const handlePrev = () => {
+    setCurrentIndex((prev: number) => {
+      return prev === 0 ? projects.length - 1 : prev - 1;
+    });
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prev) => {
+      return prev === projects.length - 1 ? 0 : prev + 1;
+    });
+  };
+
   return (
     <main id="Projects" className="relative h-screen snap-start">
       <Title num={title.num} name={title.name} />
 
       <section
         className="
-        absolute flex w-full
-        xl:top-40 xl:gap-4
+        flex w-full relative
+        xl:top-42 xl:gap-8
         lg:top-40 lg:gap-6
         md:top-40 md:gap-6
         top-32 gap-4 flex-col
     "
       >
-        {projects.map((value) => (
-          <section
-            key={value.id}
-            className="
+        <section
+          className="
             border rounded-xl border-white/10 bg-navy
-            flex flex-row px-6 py-8 gap-1 hover-project-glass
+            flex flex-row px-8 py-10 gap-1 hover-project-glass
           "
-          >
-            <div className="flex flex-col flex-1">
-              <div className="flex flex-row gap-4 items-center">
-                <h1 className="text-2xl pb-1 font-bold">{value.name}</h1>
+        >
+          <div className="flex flex-col flex-1">
+            <h1 className="text-3xl pb-3 font-bold">{currentProject.name}</h1>
 
-                <div className="flex flex-row font-fira-code pb-1 text-sm text-white/60 gap-4">
-                  <div className="flex flex-row items-center">
-                    <CiCalendar className="mr-2" />
-                    <span>{value.day}</span>
-                  </div>
-                  <span>|</span>
-                  <div className="flex flex-row items-center">
-                    <AiOutlineTeam className="mr-2" />
-                    <span>{value.type}</span>
-                  </div>
-                </div>
+            <div className="flex flex-row font-fira-code pb-2 text-sm text-white/60 gap-4">
+              <div className="flex flex-row items-center">
+                <CiCalendar className="mr-2" />
+                <span>{currentProject.day}</span>
               </div>
-
-              <p className="text-sm font-bold my-2">{value.explain}</p>
-
-              <p className="text-sm text-light-coral font-fira-code font-bold my-1">
-                Key Highlights
-              </p>
-
-              <ul>
-                {value.function.map((item, index) => (
-                  <li
-                    key={index}
-                    className="flex flex-row items-center text-xs py-1"
-                  >
-                    <IoChevronForward className="text-light-coral" />
-                    <span className="px-1">{item}</span>
-                  </li>
-                ))}
-              </ul>
+              <span>|</span>
+              <div className="flex flex-row items-center">
+                <AiOutlineTeam className="mr-2" />
+                <span>{currentProject.type}</span>
+              </div>
             </div>
 
-            <div className="flex flex-col w-[20vw] gap-2">
-              <span className="text-sm text-light-coral font-fira-code font-bold my-1">Tech Stack</span>
-              <div className="flex flex-wrap gap-2 text-xs font-fira-code">
-                {value.tech.map((tech, index) => (
-                  <span
-                    key={index}
-                    className="border border-white/20 rounded-md px-2 py-1 hover:border-light-coral  hover:text-light-coral"
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
+            <p className="text-base font-bold my-3">{currentProject.explain}</p>
 
-              <a 
-                key={value.id}
-                href={value.url}
-                target="_blank"
-                aria-label="GitHub 저장소로 이동"
-                className="
+            <p className="text-base text-light-coral font-fira-code font-bold my-2">
+              Key Highlights
+            </p>
+
+            <ul>
+              {currentProject.function.map((item, index) => (
+                <li
+                  key={index}
+                  className="flex flex-row items-center text-sm py-2"
+                >
+                  <span className="font-bold text-xl">
+                    <IoChevronForward className="text-light-coral" />
+                  </span>
+                  <span className="px-2">{item}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="flex flex-col w-[20vw] gap-2">
+            <span className="text-sm text-light-coral font-fira-code font-bold my-1">
+              Tech Stack
+            </span>
+            <div className="flex flex-wrap gap-2 text-xs font-fira-code">
+              {currentProject.tech.map((tech, index) => (
+                <span
+                  key={index}
+                  className="border border-white/20 rounded-md px-2 py-1 hover:border-light-coral  hover:text-light-coral"
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            <a
+              href={projects[currentIndex].url}
+              target="_blank"
+              aria-label="GitHub 저장소로 이동"
+              className="
                 flex w-full border mt-auto p-2 rounded-xl cursor-pointer
                 items-center justify-center hover:border-light-coral hover:text-light-coral
-              ">
-                <FaGithub />
-              </a>
-            </div>
-          </section>
-        ))}
+              "
+            >
+              <FaGithub />
+            </a>
+          </div>
+        </section>
+
+        <button
+          aria-label="previous button"
+          onClick={handlePrev}
+          className="
+            absolute -left-24 top-1/2 -translate-y-1/2 cursor-pointer
+            flex items-center justify-center border rounded-full w-12 h-12
+            hover:border-light-coral hover:text-light-coral
+          ">
+          <GrFormPrevious />
+        </button>
+        <button
+          aria-label="next button"
+          onClick={handleNext}
+          className="
+            absolute -right-24 top-1/2 -translate-y-1/2 cursor-pointer
+            flex items-center justify-center border rounded-full w-12 h-12
+            hover:border-light-coral hover:text-light-coral
+          ">
+          <GrFormNext />
+        </button>
       </section>
     </main>
   );
