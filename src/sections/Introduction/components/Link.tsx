34824063d45a02d8ac23d2eb1b3358fd4f35027b
JuHyeong424 <wkdwngud963@gmail.com>
2025-12-30 00:05:01 +0900

    refactor: links 데이터 폴더로 이동에 따른 import 추가 및 links 파일 email 빼고 참조로 수정

--- src/sections/Introduction/components/Link.tsx
+++ src/sections/Introduction/components/Link.tsx
@@ -1,28 +1,10 @@
-import { FaGithub } from "react-icons/fa";
 import { MdOutlineArrowOutward } from "react-icons/md";
-import { RiArticleLine } from "react-icons/ri";
-
-const links = [
-  {
-    label: "GitHub",
-    href: "https://github.com/JuHyeong424",
-    icon: <FaGithub />,
-    classes: "border-2 border-light-coral text-light-coral xl:py-5 xl:w-40 lg:w-35 lg:py-4 md:w-35 md:py-3 py-2 text-xs text-gradient-coral",
-    hoverClasses: "hover-glass",
-  },
-  {
-    label: "Blog",
-    href: "https://j-brothers.tistory.com/",
-    icon: <RiArticleLine />,
-    classes: "border border-navy bg-light-coral text-navy xl:py-5 xl:w-40 lg:w-35 lg:py-4 md:w-35 md:py-3 py-2 text-xs",
-    hoverClasses: "group hover-glass hover:bg-dark-coral",
-  },
-];
+import { links } from "@/app/data/myInformation";
 
 export default function Link() {
   return (
     <div className="flex md:flex-row flex-col gap-4">
-      {links.map((link) => (
+      {links.slice(0, -1).map((link) => (
         <a
           key={link.label}
           href={link.href}
